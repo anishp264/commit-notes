d2092fa34a103907b15a3027d3edf57edceeefda
anishp264 <punnenanish@gmail.com>
2023-06-07 01:12:53 +0200

    get commit messages for a pr

--- .github/actions/commitMessages/index.js
+++ .github/actions/commitMessages/index.js
@@ -1,5 +1,4 @@
 const { Octokit } = require('@octokit/rest');
-const { fs } = require('fs');
 
 async function getCommitMessages(owner, repo, pullRequestNumber) {
     const octokit = new Octokit({
@@ -24,12 +23,7 @@ async function getCommitMessages(owner, repo, pullRequestNumber) {
 // Example usage
 const owner = process.env.GITHUB_REPOSITORY.split("/")[0];
 const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
-/*const ev = JSON.parse(
-    fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
-  )
-const prNum = ev.pull_request.number
-//const pullRequestNumber = process.env.GITHUB_EVENT_PULL_REQUEST_NUMBER;
-console.log('Commit pullRequestNumber:', prNum);*/
+
 
 const githubRef = process.env.GITHUB_REF;
 const pullRequestRegex = /refs\/pull\/(\d+)\/merge/;
@@ -37,9 +31,7 @@ const pullRequestRegex = /refs\/pull\/(\d+)\/merge/;
 const match = githubRef.match(pullRequestRegex);
 const pullNumber = match ? match[1] : null;
 
-console.log('Pull Request Number:', pullNumber);
-
-getCommitMessages(owner, repo, 12)
+getCommitMessages(owner, repo, pullNumber)
   .then(commitMessages => {
     console.log('Commit messages:', commitMessages);
   })
