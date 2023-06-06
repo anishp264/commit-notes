81637d1ba69f891e15974004372e0c5c6f9be8a3
anishp264 <punnenanish@gmail.com>
2023-06-06 19:07:25 +0200

    Listing commits

--- .github/actions/commitMessages/index.js
+++ .github/actions/commitMessages/index.js
@@ -20,15 +20,40 @@ async function getCommitMessages(owner, repo, pullRequestNumber) {
   }
 }
 
+async function getCommits(owner, repo, pullRequestNumber) {
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN 
+      });
+
+  try {
+    await octokit.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: pullRequestNumber,
+    }).then(({ data }) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } catch (error) {
+    console.error('Error retrieving commit messages:', error);
+    return [];
+  }
+}
+
+
 // Example usage
 const owner = process.env.GITHUB_REPOSITORY_OWNER;
 const repo = process.env.GITHUB_REPOSITORY;
 const pullRequestNumber = process.env.GITHUB_EVENT_PULL_REQUEST_NUMBER;
+console.log('Commit pullRequestNumber:', pullRequestNumber);
 
-getCommitMessages(owner, repo, pullRequestNumber)
+/*getCommitMessages(owner, repo, pullRequestNumber)
   .then(commitMessages => {
     console.log('Commit messages:', commitMessages);
   })
   .catch(error => {
     console.error('Error:', error);
-  });
+});*/
+
