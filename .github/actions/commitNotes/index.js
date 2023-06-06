f920c69fe2bd58caf713449d5824ed1a68427f3d
anishp264 <punnenanish@gmail.com>
2023-06-06 14:31:03 +0200

    get commit msg

--- .github/actions/commitNotes/index.js
+++ .github/actions/commitNotes/index.js
@@ -1,13 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
 
-try{
-    const name = core.getInput('who-to-greet');
-    const newName = name.split("/")[1];
-    console.log(`Hello ${newName}`);
-    
-    const time = new Date();
-    core.setOutput("time", time.toTimeString());
-} catch (error){
-    core.setFailed(error.message);
-}
+async function getCommitMessage() {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN // Assumes you have a GitHub token set as a secret in your GitHub Action
+    });
+  
+    const owner = process.env.GITHUB_REPOSITORY.split("/")[0];
+    const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
+    const sha = process.env.GITHUB_SHA;
+  
+    try {
+      const commit = await octokit.repos.getCommit({
+        owner,
+        repo,
+        ref: sha
+      });
+  
+      const commitMessage = commit.data.commit.message;
+      console.log("Commit Message:", commitMessage);
+      return commitMessage;
+    } catch (error) {
+      console.error("Error retrieving commit message:", error);
+      return null;
+    }
+  }
+  
+  getCommitMessage();
