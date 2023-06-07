c714babd2881be8118da97cec9e9428fcc7c6b9a
anishp264 <punnenanish@gmail.com>
2023-06-07 16:36:41 +0200

    create commit data md content

--- .github/actions/commitNotes/index.js
+++ .github/actions/commitNotes/index.js
@@ -1,6 +1,4 @@
 const { Octokit } = require("@octokit/rest");
-const fs = require('fs');
-
 
 async function getCommitMessage() {
     const octokit = new Octokit({
@@ -19,22 +17,11 @@ async function getCommitMessage() {
       });
   
       const commitMessage = commit.data.commit.message;
-      console.log("Commit Message:", commitMessage);
-      
-
-      //New Section BEGINS
       const commitSha = commit.data.sha;
       const committerName = commit.data.commit.committer.name;
       const commitDate = commit.data.commit.committer.date;
-      
-      console.log("Commit SHA:", commitSha);    
-      console.log("Committer Name:", committerName);
-      console.log("Commit Date:", commitDate);
 
-      createCommitDataContent(commitMessage, committerName, commitDate, commitSha);
-     
-      //New Section ENDS
-
+      createCommitDataMarkDownContent(commitMessage, committerName, commitDate, commitSha);
       return commitMessage;
     } catch (error) {
       console.error("Error retrieving commit message:", error);
@@ -42,7 +29,7 @@ async function getCommitMessage() {
     }
   }
 
-  function createCommitDataContent(commitMessage, committerName, commitDate, sha){
+  function createCommitDataMarkDownContent(commitMessage, committerName, commitDate, sha){
     const markdownContent = `
     ## Commit Details
 
@@ -55,72 +42,4 @@ async function getCommitMessage() {
     console.log(`Commit Md content: => ${markdownContent}`);
   }
 
-  function createMarkDownContent(){
-    const markdownContent = `
-    # My Markdown File
-
-    This is a sample Markdown file created using JavaScript.
-
-    ## Markdown Syntax
-
-    You can use various Markdown syntax elements to format your text, such as:
-
-    - Headings
-    - Lists
-    - Bold and italic text
-    - Links
-    - Images
-    - and more...
-
-    For more information about Markdown syntax, you can refer to the [Markdown Guide](https://www.markdownguide.org/).
-
-    `;
-
-    console.log(`Md content: => ${markdownContent}`);
-
-    fs.writeFileSync("programming.md", markdownContent);
-    console.log("File written successfully\n");
-    console.log("The written has the following contents:");
-    console.log(fs.readFileSync("programming.md", "utf8"));
-  }
-
-  function createMarkDownFromArray(){
-    const data = [
-      {
-        heading: 'Introduction',
-        content: 'This is the introduction section.',
-      },
-      {
-        heading: 'Main Section',
-        content: 'This is the main content section.',
-      },
-      {
-        heading: 'Conclusion',
-        content: 'This is the conclusion section.',
-      },
-    ];
-    
-    let markdownContent = '';
-    
-    data.forEach((section) => {
-      markdownContent += `## ${section.heading}\n\n${section.content}\n\n`;
-    });
-
-    fs.writeFileSync("example.md", markdownContent);
-    console.log("Markdown file created successfully!");
-    console.log("READING FROM the example.md=>");
-    console.log(fs.readFileSync("example.md", "utf8"));
-    
-    /*fs.writeFile('example.md', markdownContent, (err) => {
-      if (err) {
-        console.error('Error creating Markdown file:', err);
-      } else {
-        console.log('Markdown file created successfully!');
-        console.log(fs.readFile("example.md", "utf8"));
-      }
-    });*/
-  }
-  
   getCommitMessage();
-  /*createMarkDownContent();
-  createMarkDownFromArray();*/
